3112d63fba5c28d0ca1664160ce61727fdd93032
Candace H-V <candace.volke@gmail.com>
2022-07-03 16:59:42 -0400

    Test if build picks up changes

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-echo "Testing out my first Jenkins job for mod 5 DevOps assignment. Now make some changes."
+echo "Testing out my first Jenkins job for mod 5 DevOps assignment. Now make some changes. Make more changes."
